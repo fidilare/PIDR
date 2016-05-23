63dfb85d6333eb3980fcb6ae07fb72bc7f55b30d
HenryTelecom <henry.ung@telecomnancy.net>
2016-05-23 21:59:28 +0200

    benchmark 3-graph

---
+++ benchmark_3_graph_3_nodes_1_in_2_out_no_cycles.m
@@ -0,0 +1,12 @@
+%%benchmark 3-graph
+%3 nodes 1 in 2 out
+
+a1 = Ark('-','x0','x1',1);
+a2 = Ark('-','x0','x1',1);
+a3 = Ark('-','x1','x0',1);
+
+a4 = Ark('-','x1','x2',1);
+a5 = Ark('-','x1','x2',1);
+a6 = Ark('-','x2','x1',1);
+
+list = {a1 a2 a3 a4 a5 a6};
