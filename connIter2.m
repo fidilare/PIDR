8c0969864971e23931107b742be3afa0472308ed
charreyr1u <jerome.charreyre@telecomnancy.eu>
2016-05-23 17:25:06 +0200

    connIter2

--- connIter2.m
+++ connIter2.m
@@ -1,27 +1,39 @@
 function [res] = connIter2(X,Y,list)
 destination = Y;
 parcouru = {};
-resteaparcourir = {};
-while resteaparcourir ~= isempty(resteaparcourir)
+resteaparcourir = {X};
+count = 0;
+while (length(resteaparcourir) ~=0 && count < length(list)*10)
     x = resteaparcourir{1};
+    parcouru = unique(parcouru);
     ListTmp = sommetAccessible(x,list);
     for s=1:length(ListTmp)
         if strcmp(ListTmp(s),Y)
             res = 1;
             return
         else 
-            for sommet=1:length(parcouru)
-                for j=1:length(resteaparcourir)
-                if (~strcmp(ListTmp(s),parcouru{sommet} &&
-                    ~strcmp(ListTmp(s),resteaparcourir{j})
-                    resteaparcourir{end+1};
+            if(length(parcouru) ~=0)
+                for sommet=1:length(parcouru)
+                    for j=1:length(resteaparcourir)
+                        if (~strcmp(ListTmp(s),parcouru{sommet}) && ~strcmp(ListTmp(s),resteaparcourir{j}))
+                            resteaparcourir{end+1} = ListTmp{s};
+                            count = count +1;
+                        end
+                    end
                 end
+            else
+                for j=1:length(resteaparcourir)
+                    if (~strcmp(ListTmp(s),resteaparcourir{j}))
+                        resteaparcourir{end+1} = ListTmp{s};
+                        count = count +1;
+                    end
                 end
             end
         end
     end
+    resteaparcourir = unique(resteaparcourir,'stable');
     resteaparcourir(1) = [];
-    parcouru{end+1};
+    parcouru{end+1} = x;
 end
 res = 0; 
                 
