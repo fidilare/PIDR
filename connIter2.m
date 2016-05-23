cffa0dd44eb98903c9b1d3112fd07876b3675660
HenryTelecom <henry.ung@telecomnancy.net>
2016-05-23 17:30:39 +0200

    update connectivite

--- connIter2.m
+++ connIter2.m
@@ -1,21 +1,21 @@
 function [res] = connIter2(X,Y,list)
 destination = Y;
 parcouru = {};
-resteaparcourir = {};
-while resteaparcourir ~= isempty(resteaparcourir)
+resteaparcourir = {X};
+while ~isempty(resteaparcourir)
     x = resteaparcourir{1};
     ListTmp = sommetAccessible(x,list);
     for s=1:length(ListTmp)
-        if strcmp(ListTmp(s),Y)
+        if strcmp(ListTmp(s),destination)
             res = 1;
             return
         else 
             for sommet=1:length(parcouru)
                 for j=1:length(resteaparcourir)
-                if (~strcmp(ListTmp(s),parcouru{sommet} &&
-                    ~strcmp(ListTmp(s),resteaparcourir{j})
-                    resteaparcourir{end+1};
-                end
+                    if (~strcmp(ListTmp(s),parcouru{sommet} &&
+                        ~strcmp(ListTmp(s),resteaparcourir{j})
+                        resteaparcourir{end+1};
+                    end
                 end
             end
         end
